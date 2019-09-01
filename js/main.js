0d4f779b91fdcb5b4ab0740615e3302949d5c65b
sjh2428 <pok_gare@naver.com>
2019-09-01 21:55:40 +0900

    가입하기 버튼 구현 완료 및 기능별 함수화 진행중

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const getClasses = () => {
         "name", "msg_name", "birthday_year", "birthday_month", "birthday_day", "msg_birthday",
         "gender", "msg_gender", "email", "msg_email", "tel", "msg_tel", "interest", "ul_interests", "input_interest",
         "msg_interest", "interest_del_btn", "reset_btn", "sign_in_btn",
-        "terms_window", "terms_window_exit_btn", "terms_window_agree_btn", "terms_check", "terms_agree_string",
-        "wrap_modal", "modal_ok_btn", "modal_cancel_btn", "modal_string"
+        "terms_window", "terms_window_exit_btn", "terms_window_agree_btn", "terms_check", "terms_agree_string", "msg_terms",
+        "wrap_modal", "modal_content", "modal_ok_btn", "modal_cancel_btn", "modal_string"
     ];
     classNames.forEach(className => {
         classObjs[className] = document.querySelector(`.${className}`);
@@ -52,88 +52,92 @@ const checkBound = (val, lower, upper) => {
 }
 
 // from_msg가 있다면 to_msg로 바꿔줌
-const changeMsgClass = (msgClass, from, to) => {
+const changeMsg = (msgClass, from, to, msgString) => {
     if (msgClass.classList.contains(from)) {
         msgClass.classList.replace(from, to);
     }
+    msgClass.innerHTML = msgString;
 }
 
-const checkId = (idValue, msgClass) => {
+const passedAllCondition = (msgClass, msgString) => {
+    changeMsg(msgClass, "err_msg", "pass_msg", msgString);
+    return true;
+}
+
+const failedCondition = (msgClass, msgString) => {
+    changeMsg(msgClass, "pass_msg", "err_msg", msgString);
+    return false;
+}
+
+const checkId = () => {
     let regex;
+    let msgString = "";
+    const idValue = classObjs["id"].value;
+    const msgClass = classObjs["msg_id"];
     if (!checkLength(idValue, 5, 20)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "5자 이상 20자 이하로 입력해주세요.";
-        return false;
+        msgString = "5자 이상 20자 이하로 입력해주세요.";
+        return failedCondition(msgClass, msgString);
     }
     regex = /^[a-z0-9-_]+$/;
     if (!idValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.";
-        return false;
+        msgString = "영문 소문자, 숫자와 특수기호(_)(-) 만 사용 가능합니다.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "사용 가능한 아이디입니다.";
-    return true;
+    msgString = "사용 가능한 아이디입니다.";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const idHandler = () => {
-    const idValue = classObjs["id"].value;
-    const idMsg = classObjs["msg_id"];
-    checkId(idValue, idMsg);
+    checkId();
 }
 
-const checkPass = (passValue, msgClass) => {
+const checkPass = () => {
     let regex;
+    let msgString = "";
+    const passValue = classObjs["pass"].value;
+    const msgClass = classObjs["msg_pass"];
     if (!checkLength(passValue, 8, 16)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "8자 이상 16자 이하로 입력해주세요.";
-        return false;
+        msgString = "8자 이상 16자 이하로 입력해주세요.";
+        return failedCondition(msgClass, msgString);
     }
     regex = /^.*[A-Z].*$/;
     if (!passValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "영문 대문자를 최소 1자 이상 포함해주세요.";
-        return false;
+        msgString = "영문 대문자를 최소 1자 이상 포함해주세요.";
+        return failedCondition(msgClass, msgString);
     }
     regex = /^.*[0-9].*$/;
     if (!passValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "숫자를 최소 1자 이상 포함해주세요.";
-        return false;
+        msgString = "숫자를 최소 1자 이상 포함해주세요.";
+        return failedCondition(msgClass, msgString);
     }
     regex = /^.*[`~!@#$%^&*()\-_+=[{\]}\\|;:'",<.>/?].*$/;
     if (!passValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "특수문자를 최소 1자 이상 포함해주세요.";
-        return false;
+        msgString = "특수문자를 최소 1자 이상 포함해주세요.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "안전한 비밀번호입니다.";
-    return true;
+    msgString = "안전한 비밀번호입니다.";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const passHandler = () => {
-    const passValue = classObjs["pass"].value;
-    const passMsg = classObjs["msg_pass"];
-    checkPass(passValue, passMsg);
+    checkPass();
 }
 
-const checkPassCheck = (passValue, passCheckValue, msgClass) => {
+const checkPassCheck = () => {
+    let msgString = "";
+    const passValue = classObjs["pass"].value;
+    const passCheckValue = classObjs["pass_check"].value;
+    const msgClass = classObjs["msg_pass_check"];
     if (passValue !== passCheckValue) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "비밀번호가 일치하지 않습니다.";
-        return false;
+        msgString = "비밀번호가 일치하지 않습니다.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "비밀번호가 일치합니다.";
-    return true;
+    msgString = "비밀번호가 일치합니다.";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const passCheckHandler = () => {
-    const passValue = classObjs["pass"].value;
-    const passCheckValue = classObjs["pass_check"].value;
-    const passCheckMsg = classObjs["msg_pass_check"];
-    checkPassCheck(passValue, passCheckValue, passCheckMsg);
+    checkPassCheck();
 }
 
 const getLastDay = (yearValue, monthValue) => {
@@ -146,101 +150,110 @@ const getLastDay = (yearValue, monthValue) => {
     return dateObjForLastDay.getDate();
 }
 
-const checkBirth = (yearValue, monthValue, dayValue, msgClass) => {
+const checkName = () => {
+    let msgString = "";
+    const nameValue = classObjs["name"].value;
+    const msgClass = classObjs["msg_name"];
+    if (nameValue.length < 2) {
+        msgString = "2글자 이상의 이름을 입력해주세요.";
+        return failedCondition(msgClass, msgString);
+    }
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
+}
+
+const nameHandler = () => {
+    checkName();
+}
+
+const checkBirth = () => {
+    let msgString = "";
+    let yearValue = classObjs["birthday_year"].value;
+    let monthValue = classObjs["birthday_month"].value;
+    let dayValue = classObjs["birthday_day"].value;
+    const msgClass = classObjs["msg_birthday"];
     // year handle
     const nowYear = new Date().getFullYear();
     const lowerBound = nowYear - 14;
     const upperBound = lowerBound + 99;
     if (isNaN(Number(yearValue)) || yearValue.length !== 4) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "태어난 년도 4자리를 정확하게 입력하세요.";
-        return false;
+        msgString = "태어난 년도 4자리를 정확하게 입력하세요.";
+        return failedCondition(msgClass, msgString);
     }
     yearValue = Number(yearValue);
     if (checkBound(yearValue, lowerBound, upperBound)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "만 14세 이상 만 99세 이하만 가입 가능합니다.";
-        return false;
+        msgString = "만 14세 이상 만 99세 이하만 가입 가능합니다.";
+        return failedCondition(msgClass, msgString);
     }
     // month handle
     monthValue = Number(monthValue);
     if (isNaN(monthValue)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "태어난 월을 선택해주세요";
-        return false;
+        msgString = "태어난 월을 선택해주세요";
+        return failedCondition(msgClass, msgString);
     }
     // day handle
     dayValue = Number(dayValue);
     lastDay = getLastDay(yearValue, monthValue);
     if (isNaN(dayValue) || !checkBound(dayValue, 1, lastDay)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = `태어난 날짜를 다시 확인해주세요. ${yearValue}년 ${monthValue}월은 1일부터 ${lastDay}까지 존재합니다.`;
-        return false;
+        msgString = `태어난 날짜를 다시 확인해주세요. ${yearValue}년 ${monthValue}월은 1일부터 ${lastDay}까지 존재합니다.`;
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "&nbsp;";
-    return true;
+    msgString = "&nbsp;"
+    return passedAllCondition(msgClass, msgString);
 }
 
 const birthHandler = () => {
-    const yearValue = classObjs["birthday_year"].value;
-    const monthValue = classObjs["birthday_month"].value;
-    const dayValue = classObjs["birthday_day"].value;
-    const birthMsg = classObjs["msg_birthday"];
-    checkBirth(yearValue, monthValue, dayValue, birthMsg);
+    checkBirth();
 }
 
-const checkGender = (genderValue, msgClass) => {
+const checkGender = () => {
+    let msgString = "";
+    const genderValue = classObjs["gender"].value;
+    const msgClass = classObjs["msg_gender"];
     if (genderValue === "성별") {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "성별을 선택해주세요";
-        return false;
+        msgString = "성별을 선택해주세요";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "&nbsp;";
-    return true;
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const genderHandler = () => {
-    const genderValue = classObjs["gender"].value;
-    const genderMsg = classObjs["msg_gender"];
-    checkGender(genderValue, genderMsg);
+    checkGender();
 }
 
-const checkEmail = (emailValue, msgClass) => {
+const checkEmail = () => {
+    let msgString = "";
+    const emailValue = classObjs["email"].value;
+    const msgClass = classObjs["msg_email"];
     const regex = /^[A-Za-z0-9]([-_\.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
     if (!emailValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "이메일 주소를 다시 확인해주세요.";
-        return false;
+        msgString = "이메일 주소를 다시 확인해주세요.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "&nbsp;";
-    return true;
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const emailHandler = () => {
-    const emailValue = classObjs["email"].value;
-    const emailMsg = classObjs["msg_email"];
-    checkEmail(emailValue, emailMsg);
+    checkEmail();
 }
 
-const checkTel = (telValue, msgClass) => {
+const checkTel = () => {
+    let msgString = "";
     const regex = /^010[0-9]{7,8}$/;
+    const telValue = classObjs["tel"].value;
+    const msgClass = classObjs["msg_tel"];
     if (!telValue.match(regex)) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "형식에 맞지 않는 번호입니다.";
-        return false;
+        msgString = "형식에 맞지 않는 번호입니다.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "&nbsp;";
-    return true;
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const telHandler = () => {
-    const telValue = classObjs["tel"].value;
-    const telMsg = classObjs["msg_tel"];
-    checkTel(telValue, telMsg);
+    checkTel();
 }
 
 const lastInterestEdit = (e) => {
@@ -254,7 +267,7 @@ const lastInterestEdit = (e) => {
 }
 
 const inputKeyIsBackspace = (e) => {
-    return e.keyCode === 8;
+    return e.key === "Backspace";
 }
 
 const valueIsEmpty = (e) => {
@@ -262,7 +275,7 @@ const valueIsEmpty = (e) => {
 }
 
 const lastInputIsComma = (e) => {
-    return e.target.value[e.target.value.length - 1] === ",";
+    return e.key === ",";
 }
 
 const getInterestNode = (str) => {
@@ -288,38 +301,48 @@ const appendInterest = (e) => {
     e.target.value = "";
 }
 
-const checkInterest = (interestsCnt, msgClass) => {
+const checkInterest = () => {
+    let msgString = "";
+    const interestsCnt = classObjs["ul_interests"].childNodes.length;
+    const msgClass = classObjs["msg_interest"];
     if (interestsCnt < 3) {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "3개 이상의 관심사를 입력하세요.";
-        return false;
+        msgString = "3개 이상의 관심사를 입력하세요.";
+        return failedCondition(msgClass, msgString);
     }
-    changeMsgClass(msgClass, "err_msg", "pass_msg");
-    msgClass.innerHTML = "&nbsp;";
-    return true;
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const interestHandler = (e) => {
-    // e.keyCode
-    // 188: ,(comma)와 <(꺽쇄?)도 또한 188이기 때문에
-    //      마지막 문자열을 체크하는 방식으로 해야할 듯 함
-    // 8: backspace
+    // e.key를 권장하는 mdn
+    // ref. slack에서 최형준님 말씀 참조
     if (inputKeyIsBackspace(e) && valueIsEmpty(e)) {
         lastInterestEdit(e);
     }
     if (lastInputIsComma(e)) {
         appendInterest(e);
     }
-    const interestsCnt = classObjs["ul_interests"].childNodes.length;
-    const interestMsg = classObjs["msg_interest"];
-    checkInterest(interestsCnt, interestMsg);
+    checkInterest();
 }
 
 const interestDelHandler = (e) => {
     e.target.parentNode.remove();
-    const interestsCnt = classObjs["ul_interests"].childNodes.length;
-    const interestMsg = classObjs["msg_interest"];
-    checkInterest(interestsCnt, interestMsg);
+    checkInterest();
+}
+
+const notAgreedTerms = () => {
+    return !classObjs["terms_check"].checked;
+}
+
+const checkTerms = () => {
+    let msgString = "";
+    const msgClass = classObjs["msg_terms"];
+    if (notAgreedTerms()) {
+        msgString = "약관에 동의해주세요.";
+        return failedCondition(msgClass, msgString);
+    }
+    msgString = "&nbsp;";
+    return passedAllCondition(msgClass, msgString);
 }
 
 const termsHandler = () => {
@@ -342,36 +365,92 @@ const resetBtnHandler = () => {
     classObjs["modal_cancel_btn"].addEventListener("click", resetCancelBtnHandler);
 }
 
-const resetOkBtnHandler = () => {
+const initializeAllInputData = () => {
     classObjs["sign_up_form"].reset();
-    classObjs["modal_string"].innerHTML = "";
-    document.querySelectorAll(".msg").forEach(msgClass => {
-        changeMsgClass(msgClass, "pass_msg", "err_msg");
-        msgClass.innerHTML = "&nbsp";
-    });
     classObjs["ul_interests"].innerHTML = "";
+}
+
+const initializeAllMsgClass = () => {
+    document.querySelectorAll(".msg").forEach(msgClass => {
+        changeMsg(msgClass, "pass_msg", "err_msg", "&nbsp");
+    });
+}
+
+const removeResetFeaturesInModal = () => {
     classObjs["wrap_modal"].style.display = "none";
+    classObjs["modal_string"].innerHTML = "";
     classObjs["modal_ok_btn"].removeEventListener("click", resetOkBtnHandler);
     classObjs["modal_cancel_btn"].removeEventListener("click", resetCancelBtnHandler);
+}
+
+const resetOkBtnHandler = () => {
+    removeAllInputData();
+    initializeAllMsgClass();
+    removeResetFeaturesInModal();
 }
 
 const resetCancelBtnHandler = () => {
+    removeResetFeaturesInModal();
+}
+
+const createFulfillMsgList = () => {
+    const fulfillMsg = [];
+    checkList.forEach(list => {
+        if (!list.func()) {
+            fulfillMsg.push(list.msg);
+        }
+    });
+    return fulfillMsg;
+}
+
+const fulfillModalOkBtnHandler = () => {
     classObjs["wrap_modal"].style.display = "none";
-    classObjs["modal_string"].innerHTML = "";
-    classObjs["modal_ok_btn"].removeEventListener("click", resetOkBtnHandler);
-    classObjs["modal_cancel_btn"].removeEventListener("click", resetCancelBtnHandler);
+    classObjs["modal_ok_btn"].style.width = "130px";
+    classObjs["modal_cancel_btn"].style.display = "flex";
+    classObjs["modal_string"].innerHTML += "";
+    classObjs["modal_content"].style.height = "200px";
+    classObjs["modal_ok_btn"].removeEventListener("click", fulfillModalOkBtnHandler);
+}
+
+const removeCancelBtnInModal = () => {
+    classObjs["modal_cancel_btn"].style.display = "none";
+}
+
+const expandOkBtnInModal = () => {
+    classObjs["modal_ok_btn"].style.width = "100%";
+}
+
+const makeFulfillMsg = (fulfillMsgList) => {
+    let fulfillMsg = "";
+    fulfillMsgList.forEach(msg => {
+        fulfillMsg += msg + "<br>";
+    });
+    return fulfillMsg;
 }
 
 const signInBtnHandler = () => {
-    
+    const fulfillMsgList = createFulfillMsgList();
+    if (fulfillMsg.length === 0) { // 전체 입력 form이 올바르게 입력되었다면
+        // fetch등을 사용하여 회원가입 진행
+        // -----------
+        // code ..
+        // -----------
+    } else { // 전체 입력 form이 올바르게 입력되지 않았다면
+        // modal을 사용하여 입력하라 알려줌
+        removeCancelBtnInModal();
+        expandOkBtnInModal();
+        classObjs["modal_string"].innerHTML = makeFulfillMsg(fulfillMsgList);
+        classObjs["wrap_modal"].style.display = "flex";
+        classObjs["modal_content"].style.height = `${200 + ((fulfillMsgList.length - 1) * 20)}px`;
+        classObjs["modal_ok_btn"].addEventListener("click", fulfillModalOkBtnHandler);
+    }
 }
 
-const init = () => {
-    getClasses();
-    injectFocusEvent();
+const injectEventListener = () => {
     classObjs["id"].addEventListener("keyup", idHandler);
     classObjs["pass"].addEventListener("keyup", passHandler);
     classObjs["pass_check"].addEventListener("keyup", passCheckHandler);
+    classObjs["name"].addEventListener("keyup", nameHandler);
     classObjs["birthday_year"].addEventListener("keyup", birthHandler);
     classObjs["birthday_month"].addEventListener("change", birthHandler);
     classObjs["birthday_month"].addEventListener("keyup", birthHandler);
@@ -388,5 +467,45 @@ const init = () => {
     classObjs["reset_btn"].addEventListener("click", resetBtnHandler);
     classObjs["sign_in_btn"].addEventListener("click", signInBtnHandler);
 }
+
+const init = () => {
+    getClasses();
+    injectFocusEvent();
+    injectEventListener();
+}
+
+const checkList = [
+    {
+        func: checkId,
+        msg: "아이디를 확인해주세요."
+    }, {
+        func: checkPass,
+        msg: "비밀번호를 확인해주세요."
+    }, {
+        func: checkPassCheck,
+        msg: "비밀번호 재확인란을 확인해주세요."
+    }, {
+        func: checkName,
+        msg: "이름을 확인해주세요."
+    }, {
+        func: checkBirth,
+        msg: "생년월일을 확인해주세요."
+    }, {
+        func: checkGender,
+        msg: "성별을 선택해주세요."
+    }, {
+        func: checkEmail,
+        msg: "이메일을 확인해주세요."
+    }, {
+        func: checkTel,
+        msg: "전화번호를 확인해주세요."
+    }, {
+        func: checkInterest,
+        msg: "관심사를 확인해주세요."
+    }, {
+        func: checkTerms,
+        msg: "약관에 동의해주세요."
+    }
+]
 
 init();
