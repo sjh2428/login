db005169d3696ff0acb53f7a50d6be0aa427bd1c
sjh2428 <pok_gare@naver.com>
2019-09-05 17:43:02 +0900

    feat: add userModel using User class

---
+++ model/User.js
@@ -0,0 +1,14 @@
+module.exports = class {
+    constructor(info) {
+        this.id = info.id;
+        this.pwd = info.pwd;
+        this.name = info.name;
+        this.birthYear = info.birthYear;
+        this.birthMonth = info.birthMonth;
+        this.birthDay = info.birthDay;
+        this.gender = info.gender;
+        this.email = info.email;
+        this.tel = info.tel;
+        this.interests = info.interest;
+    }
+}
---
+++ model/userModel.js
@@ -0,0 +1,42 @@
+const lowdb = require("lowdb");
+const fileSync = require("lowdb/adapters/FileSync");
+const adapter = new fileSync("./model/db.json");
+const uuidv1 = require("uuid/v1");
+
+const User = require("./User");
+
+class UserModel {
+    constructor() {
+        this.db = lowdb(adapter);
+        this.db.defaults({
+            "adminkey": {
+                "id": "admin",
+                "pwd": "1q2w3e4r!@",
+                "name": "어드민",
+                "birthDay": 12,
+                "birthMonth": 3,
+                "birthYear": 1,
+                "gender": "man",
+                "email": "qwer@naver.com",
+                "tel": "01044444444",
+                "interests": ["interest1", "interest2", "interest3"]
+            }
+        }).write();
+    }
+    createUser(info, key = uuidv1()) {
+        this.db.set(key, new User(info)).write();
+        this.db.get(key).assign({interests: info.interests}).write();
+    }
+    findUser(key) {
+        return this.db.get(key).value();
+    }
+    updateUser(key, newInfo) {
+        this.db.set(key, newInfo).write();
+        this.db.get(key).assign({interests: newInfo.interests}).write();
+    }
+    deleteUser(key) {
+        this.db.unset(key).write();
+    }
+}
+
+module.exports = new UserModel();
